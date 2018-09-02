56f81539fc86ccb4c20d81239cb140656938228b
Mark Kuczmarski <markkuczmarski@gmail.com>
2018-09-02 16:53:46 +0900

    Got trezor to be owner

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,5 @@
 const C50 = artifacts.require("./C50V2.sol");
 
 module.exports = function(deployer, network, accounts) {
-	return deployer.deploy(C50, accounts[1], {from: "0x171a848F509eF3d9c150F82114d94E26eA6c4A0E"});
+	return deployer.deploy(C50, "0x3ff32898abff9c57e3ae04ca4a3565d4c81b209e", {from: "0xd6ea58a0149400e6d35b3b0e3e06ff20b8479cb0"});
 }
--- truffle.js
+++ truffle.js
@@ -1,7 +1,7 @@
 // Allows us to use ES6 in our migrations and tests.
 require('babel-register')
 let HDWalletProvider = require("truffle-hdwallet-provider");
-let mnemonic = "grape canyon dog usual deal artist burst advice mix innocent hood unique";
+let mnemonic = "wrestle outdoor refuse wrong breeze black sea soon salmon cricket suspect improve";
 var infura_apikey = "7dzkhERjQJsad7aNgAHw"; 
 
 //https://truffleframework.com/docs/truffle/reference/configuration
